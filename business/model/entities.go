ac5db910835d8e667e0ce9399fc75af2a266d1a2
Lucas Godoy <lucas.godoy@rappi.com>
2020-08-12 15:49:43 -0300

    Beer pack calculator

---
+++ business/model/entities.go
@@ -0,0 +1,27 @@
+package model
+
+import (
+	"time"
+
+	"github.com/jinzhu/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Username string   `gorm:"index;not null" validate:"email"`
+	Password string   `gorm:"not null"`
+	Scopes   []*Scope `gorm:"many2many:retailer_scopes"`
+}
+
+type Scope struct {
+	gorm.Model
+	Name        string `gorm:"index;unique;not null"`
+	Description string
+}
+
+type MeetUp struct {
+	gorm.Model
+	Name        string     `gorm:"not null"`
+	Description string     `gorm:"not null"`
+	DateOn      *time.Time `gorm:"index;not null"`
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/lucas-dev-it/62252aee-9d11-4149-a0ea-de587cbcd233
+
+go 1.14
+
+require github.com/jinzhu/gorm v1.9.15 // indirect
